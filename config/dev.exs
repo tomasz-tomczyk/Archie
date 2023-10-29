6f1057f011aa83b93a6d96cfb0f7ff0f1099ee43
Tomasz Tomczyk <me@tomasztomczyk.com>
2023-10-29 20:19:13 +0000

    Configuration

--- config/dev.exs
+++ config/dev.exs
@@ -2,7 +2,7 @@ import Config
 
 # Configure your database
 config :archie, Archie.Repo,
-  database: Path.expand("../archie_dev.db", Path.dirname(__ENV__.file)),
+  database: Path.expand("../data/archie_dev.db", Path.dirname(__ENV__.file)),
   pool_size: 5,
   stacktrace: true,
   show_sensitive_data_on_connection_error: true
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Archie.MixProject do
     [
       app: :archie,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
